435dbd93153fcae52b3928441fcb41e6a7eb4d79
Jay Whang and Taylor Grohs <taylorgrohs@gmail.com>
2016-01-20 11:53:18 -0800

    add slice for qu

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,37 @@
 var pigLatin = function(word){
-  var example = search(word);
-  return word.slice(example) + word.slice(0, example) + "ay";
+  var slicePostion = search(word); // assigns number generaged by search function
+  if(word[0] === "q") {
+    return word.slice(2) + word.slice(0, 2) + "ay";
+  } else {
+    return word.slice(slicePostion) + word.slice(0, slicePostion) + "ay"; //first slice removes and save letters from certain/target index to end of length of array, second slice removes and save letters from 0 index to certain/target index.
+    }
 };
 
 var search = function(word) {
   var  word = word.split("");
   var vowels = ["a", "e", "i", "o", "u"]
 
-for (var j = 0; j < word.length; j++) {
-  for (var i = 0; i < vowels.length; i++) {
-    if (word[j]  === vowels[i]) {
-      return j;
+  for (var j = 0; j < word.length; j++) {
+    for (var i = 0; i < vowels.length; i++) {
+      if (word[j]  === vowels[i]) {
+        return j;     // returns number (j-eth number);
+      }
     }
   }
-}
 };
-  $(document).ready(function(){
-    $("form#pigLatin").submit(function(event){
-      var word = $("input#word").val();
-      var validate = pigLatin(word);
-      debugger;
-      if (validate) {
-        $(".result").text(pigLatin(word));
-      }
-      else {
-        alert("error");
-      }
 
 
+$(document).ready(function(){
+  $("form#pigLatin").submit(function(event){
+    var word = $("input#word").val();
+    var validate = pigLatin(word);
+    //debugger;
+    if (validate) {
+      $(".result").text(pigLatin(word));
+    }
+    else {
+      alert("error");
+    }
     event.preventDefault();
   });
 });
